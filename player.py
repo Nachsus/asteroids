5374b42f4881b626ec6a1e7dcad4678c5e1215aa
Nachsus <polodono@gmail.com>
2025-12-19 22:04:35 +0100

    shot cd

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame
 from shot import Shot
 
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cd = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,6 +37,8 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+        self.shot_cd -= dt
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
@@ -43,8 +46,12 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        shot = Shot(self.position[0], self.position[1])
-        unit = pygame.Vector2(0, 1)
-        rotated = unit.rotate(self.rotation)
-        scaled = rotated * PLAYER_SHOOT_SPEED
-        shot.velocity = scaled
+        if self.shot_cd > 0:
+            pass
+        else:
+            shot = Shot(self.position[0], self.position[1])
+            unit = pygame.Vector2(0, 1)
+            rotated = unit.rotate(self.rotation)
+            scaled = rotated * PLAYER_SHOOT_SPEED
+            shot.velocity = scaled
+            self.shot_cd = PLAYER_SHOOT_COOLDOWN_SECONDS
